795c49ecfd5a18e2529940f9d7c24f0d6534a8e4
moumitabhattacharya <moumitab@udel.edu>
2016-08-07 16:45:06 -0400

    commit to GitHub

---
+++ DataAnalysis/GitTryOut.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn import svm
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn import cross_validation
+from sklearn.dummy import DummyClassifier
+
+print "============================================="
+print " Results of optimization "
+print "============================================="
+print "Dummy Mean R^2: "
+print "Naive Bayes Mean R^2: "
+print "SVM Mean R^2"
